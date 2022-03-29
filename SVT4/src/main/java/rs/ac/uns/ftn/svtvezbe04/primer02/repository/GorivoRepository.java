f4050cce8fe7bbccd59642a57d5bd634afe12e50
n-dusan <nikolic.dusan.dey@gmail.com>
2022-03-29 12:04:30 +0200

    include v4 jpa-mysql

--- SVT4/src/main/java/rs/ac/uns/ftn/svtvezbe04/primer02/repository/GorivoRepository.java
+++
@@ -1,9 +0,0 @@
-package rs.ac.uns.ftn.svtvezbe04.primer02.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import rs.ac.uns.ftn.svtvezbe04.primer02.entity.Gorivo;
-
-@Repository
-public interface GorivoRepository extends JpaRepository<Gorivo, Integer> {
-}
