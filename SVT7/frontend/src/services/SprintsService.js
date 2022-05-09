0eb65484cefa6b33313546498dbce0ee8b1a8054
n-dusan <nikolic.dusan.dey@gmail.com>
2022-05-10 00:24:13 +0200

    include: v7

--- SVT7/frontend/src/services/SprintsService.js
+++
@@ -1,9 +0,0 @@
-import SprintsAxiosClient from "./clients/SprintsAxiosClient";
-
-export const SprintsService = {
-  getSprints,
-};
-
-async function getSprints() {
-  return await SprintsAxiosClient.get("sprints");
-}
--- SVT7/frontend/src/services/StatesService.js
+++
@@ -1,9 +0,0 @@
-import SprintsAxiosClient from "./clients/SprintsAxiosClient";
-
-export const StatesService = {
-  getStates,
-};
-
-async function getStates() {
-  return await SprintsAxiosClient.get("states");
-}
