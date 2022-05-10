db5da61a1381d1a5893060c51589bd4633ba81f6
n-dusan <nikolic.dusan.dey@gmail.com>
2022-05-10 11:40:37 +0200

    v6: omit oauth

--- SVT6/Vezbe6OAuth/src/main/java/rs/ac/uns/ftn/svtvezbe06oauth/repository/ClubRepository.java
+++
@@ -1,9 +0,0 @@
-package rs.ac.uns.ftn.svtvezbe06oauth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import rs.ac.uns.ftn.svtvezbe06oauth.model.Club;
-
-@Repository
-public interface ClubRepository extends JpaRepository<Club, Long> {
-}
